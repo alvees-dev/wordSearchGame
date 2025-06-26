1caf4e7ab45f8df360cc81ac3de8700cc656c62b
Gabriel Alves <alvesg.dev@gmail.com>
2025-06-25 21:00:41 -0300

    create board class, filling matrix

--- src/main/java/br/com/wordsearchgame/Main.java
+++ src/main/java/br/com/wordsearchgame/Main.java
@@ -1,7 +1,13 @@
 package br.com.wordsearchgame;
 
+import br.com.wordsearchgame.grid.Board;
+
 public class Main {
+	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	
+        Board board = new Board(10);
+        board.printMatrix();
+        
     }
 }
---
+++ src/main/java/br/com/wordsearchgame/grid/Board.java
@@ -0,0 +1,66 @@
+package br.com.wordsearchgame.grid;
+
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Board {
+
+	private static final Logger logger = LoggerFactory.getLogger(Board.class);
+
+	private int size;
+	private char[][] matrix;
+	private Random random;
+
+	public Board(int size) {
+		this.size = size;
+		this.matrix = new char[size][size];
+		this.random = new Random();
+		createMatrix();
+		fillingMatrix();
+	}
+
+	private void createMatrix() {
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				matrix[i][j] = ' ';
+			}
+		}
+	}
+
+	private void fillingMatrix() {
+		for (int i = 1; i < size; i++) {
+			for (int j = 1; j < size; j++) {
+				if (matrix[i][j] == ' ') {
+					matrix[i][j] = (char) ('A' + random.nextInt(26));
+
+				}
+			}
+		}
+	}
+
+	public void printMatrix() {
+
+		System.out.print("  ");
+
+		// Coloca os números das colunas
+		for (int i = 1; i <= size; i++) {
+			System.out.printf("%2d ", i);
+		}
+		System.out.println();
+
+		// Coloca as letras das linhas
+		for (int i = 0; i < size; i++) {
+			System.out.print((char) ('A' + i) + "  ");
+
+			for(int j = 0; j < size; j++) {
+				System.out.print(matrix[i][j] + "  ");
+			}
+			System.out.println();
+
+		}
+	}
+
+}
